39677303bae159b2bc6948ab4768146bea9027b4
RedHellier <redhellier@gmail.com>
2025-07-17 15:42:07 +0100

    Backgrounds and Fonts sorted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<section class="container">
+	<div class="highlighted">
+		<h1>HIGHLIGHTED</h1>
+	</div>
+
+	<h1>HEADER ONE</h1>
+	<h2>HEADER TWO</h2>
+	<p>PARAGRAPH</p>
+</section>
+
+<style>
+	:global(body) {
+		background-image: url(/WebBackgrounds/ContactBackground.jpg);
+	}
+</style>
